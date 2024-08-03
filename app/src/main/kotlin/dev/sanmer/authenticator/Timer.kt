9d5b38cae3836e31840bbdc36d13a1d4dff085a2
SanmerDev <sanmer@sanmer.dev>
2024-08-03 16:30:30 +0800

    New `Timer`

--- app/src/main/kotlin/dev/sanmer/authenticator/Timer.kt
+++ app/src/main/kotlin/dev/sanmer/authenticator/Timer.kt
@@ -3,55 +3,57 @@ package dev.sanmer.authenticator
 import dev.sanmer.ntp.NtpServer
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import kotlin.time.Duration.Companion.seconds
 
-object Timer {
-    private val coroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+interface Timer {
+    val epochSeconds: StateFlow<Long>
+    fun start()
+    fun stop()
 
-    private val epochSecondsFlow = MutableStateFlow(System.currentTimeMillis())
-    val epochSeconds get() = epochSecondsFlow.asStateFlow()
+    companion object Default : Timer {
+        private val coroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+        private var job: Job? = null
+        private var offset = 0L
 
-    private var offset = 0L
+        override val epochSeconds = MutableStateFlow(System.currentTimeMillis())
 
-    init {
-        sync()
-    }
-
-    fun start() {
-        coroutineScope.launch {
-            epochSeconds().collect(epochSecondsFlow)
-        }
-    }
-
-    fun stop() {
-        coroutineScope.cancel()
-    }
-
-    private fun sync() {
-        coroutineScope.launch {
-            runCatching {
-                offset = NtpServer.Apple.sync()
-                Timber.d("Time offset: $offset")
-            }.onFailure {
-                Timber.e(it)
+        override fun start() {
+            sync()
+            job = coroutineScope.launch {
+                epochSeconds().collect(epochSeconds)
             }
         }
-    }
 
-    private fun epochSeconds() = flow {
-        while (currentCoroutineContext().isActive) {
-            emit((System.currentTimeMillis() + offset) / 1000)
-            delay(1.seconds)
+        override fun stop() {
+            job?.cancel()
+        }
+
+        private fun sync() {
+            coroutineScope.launch {
+                runCatching {
+                    offset = NtpServer.Apple.sync()
+                    Timber.d("Time offset: $offset")
+                }.onFailure {
+                    Timber.e(it)
+                }
+            }
+        }
+
+        private fun epochSeconds() = flow {
+            while (currentCoroutineContext().isActive) {
+                emit((System.currentTimeMillis() + offset) / 1000)
+                delay(1.seconds)
+            }
         }
     }
 }
