24212dfcd4c1d9434402fdb82ad87805854db111
SanmerDev <sanmer@sanmer.dev>
2024-08-22 21:10:10 +0800

    Optimize `HOTP`

--- core/src/main/kotlin/dev/sanmer/otp/ByteArray.kt
+++ core/src/main/kotlin/dev/sanmer/otp/ByteArray.kt
@@ -6,6 +6,7 @@ import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
+import kotlin.math.pow
 
 internal inline fun Long.toByteArray() =
     ByteBuffer.allocate(Long.SIZE_BYTES)
@@ -24,3 +25,13 @@ internal inline fun ByteArray.hmacSha1(key: ByteArray) = hmac(key, "HmacSHA1")
 internal inline fun ByteArray.hmacSha256(key: ByteArray) = hmac(key, "HmacSHA256")
 
 internal inline fun ByteArray.hmacSha512(key: ByteArray) = hmac(key, "HmacSHA512")
+
+internal inline fun ByteArray.otp(digits: Int): Int {
+    val offset = this[size - 1].toInt() and 0x0F
+    val code = ((this[offset].toInt() and 0x7F shl 24)
+            or (this[offset + 1].toInt() and 0xFF shl 16)
+            or (this[offset + 2].toInt() and 0xFF shl 8)
+            or (this[offset + 3].toInt() and 0xFF))
+
+    return code % 10.0.pow(digits).toInt()
+}
